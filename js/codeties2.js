0394e1ac86304a5117b6c99c94fff2bb79f1e614
Ties Noordhuis <tiesnoordhuis@gmail.com>
2016-12-12 17:07:25 +0100

    char en key mode gemaak

--- js/codeties2.js
+++ js/codeties2.js
@@ -1,6 +1,31 @@
 var test = "<button onclick=\"buttonClear()\">clear</button>";
 var charLogger = [];
-var lijst = [];
+var keyLogger = [];
+var charLijst = [];
+var keyLijst = [];
+var n = 1;
+
+function charKeyMode(n){
+	if (n==1){
+		document.getElementById("charWindow").style.display = "block";
+		document.getElementById("keyWindow").style.display = "none";
+	} else if (n==2){
+		document.getElementById("charWindow").style.display = "none";
+		document.getElementById("keyWindow").style.display = "block";
+	} else {
+		document.getElementById("charWindow").style.display = "block";
+		document.getElementById("keyWindow").style.display = "block";
+		window.alert("fout in charKeyMode");
+	}
+}
+
+
+
+
+function onAllKeyDown(event){
+	var x = event.which || event.keyCode;
+	document.getElementById("keyContainer1").innerHTML = x;
+}
 
 function onAllKeyPress(event){
 	var x = event.which || event.keyCode;
@@ -26,8 +51,8 @@ function buttonClear(){
 
 function makeList(){
 	for (i = 0; i < charLogger.length; i ++){
-		lijst.push(String.fromCharCode(charLogger[i]));
-		document.getElementById("mainContainer5").innerHTML = lijst;
+		charLijst.push(String.fromCharCode(charLogger[i]));
+		document.getElementById("mainContainer5").innerHTML = charLijst;
 	} 
-lijst = []
+charLijst = []
 }
