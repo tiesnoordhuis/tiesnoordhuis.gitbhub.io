6643494b25a9c75c6663b161ccf25395d43fc15d
Ties Noordhuis <tiesnoordhuis@gmail.com>
2016-12-12 16:45:56 +0100

    javascript keylogger

--- js/codeties2.js
+++ js/codeties2.js
@@ -1,5 +1,33 @@
-var test = "test";
+var test = "<button onclick=\"buttonClear()\">clear</button>";
+var charLogger = [];
+var lijst = [];
+
+function onAllKeyPress(event){
+	var x = event.which || event.keyCode;
+	document.getElementById("mainContainer3").innerHTML = x;
+	charLogger.push(x);
+}
 
 function startFunction(){
-	document.getElementById("mainContainer").innerHTML = test;
+	var h = [window.innerHeight,window.innerWidth,0,0] ;
+	document.getElementById("mainContainer").innerHTML += test;
+	
+}
+
+function buttonFunction(){
+	document.getElementById("mainContainer2").innerHTML += charLogger;
+	document.getElementById("mainContainer2").innerHTML += "<br>";
+}
+
+function buttonClear(){
+	document.getElementById("mainContainer2").innerHTML = "";
+	charLogger = [];
+}
+
+function makeList(){
+	for (i = 0; i < charLogger.length; i ++){
+		lijst.push(String.fromCharCode(charLogger[i]));
+		document.getElementById("mainContainer5").innerHTML = lijst;
+	} 
+lijst = []
 }
